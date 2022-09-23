6f904c3c36f419d5a52a942cc1accb385f614e36
Samantha <103006757+SamanthaGitHub@users.noreply.github.com>
2022-09-23 10:23:26 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Systems!\nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
