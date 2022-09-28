197ad2cd0f25c284e312b409c3bbf439e97bb0b1
Samantha <samanthax0326@gmail.com>
2022-09-28 15:43:03 -0700

    BooleanLogic
    step 199

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checks applicants age and prints to the console
+            Console.WriteLine("Car Insurance Application\nWhat is your age?");
+            string stringage = Console.ReadLine();
+            byte age = Convert.ToByte(stringage);
+            Console.WriteLine(age);
+
+            // Checks applicants DUIs and prints to the console
+            Console.WriteLine("True or False: Have you ever gotten a DUI?");
+            string stringduis = Console.ReadLine();
+            bool duis = Convert.ToBoolean(stringduis);
+            Console.WriteLine(duis);
+
+            // Checks applicants tickets and prints to the console
+            Console.WriteLine("How many speeding tickets do you have?");
+            string stringtickets = Console.ReadLine();
+            byte tickets = Convert.ToByte(stringtickets);
+            Console.WriteLine(tickets);
+
+            // Displays whether or not applicannt is eligible based on above criteria
+            Console.WriteLine("Qualified?");
+            if (age >= 16 && duis == false && tickets <= 3)
+            {
+                bool qualified = true;
+                Console.WriteLine(qualified);
+                Console.ReadLine();
+            }
+            else
+            {
+                bool q = false;
+                Console.WriteLine(q);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
