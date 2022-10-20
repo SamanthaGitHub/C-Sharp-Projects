255cedcb6215d05354d4ae7bbccc6e3b75e5797e
Samantha <samanthax0326@gmail.com>
2022-10-19 17:53:44 -0700

    Calling methods

---
+++ CallingMethods/CallingMethods/Maths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Maths
+    {
+        //divides input by 2
+        public static decimal DivideByTwo(decimal input)
+        {
+            decimal result = input / 2;
+            return result;
+        }
+
+        //multiplies input by 2
+        public static int MultiplyByTwo(int input2)
+        {
+            int result2 = input2 * 2;
+            return result2;
+        }
+
+        //adds two to input
+        public static int AddTwo(int input3)
+        {
+            int result3 = input3 + 2;
+            return result3;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //divide by 2
+            Console.WriteLine("Enter a number to be divided by two:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            decimal divide2 = Maths.DivideByTwo(input);
+            Console.WriteLine(divide2);
+            
+
+            //multiply by 2
+            Console.WriteLine("Enter a number to be multiplied by two:");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int multiply2 = Maths.MultiplyByTwo(input2);
+            Console.WriteLine(multiply2);
+
+            //add to 2
+            Console.WriteLine("Enter a number to be added to two:");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            int add2 = Maths.AddTwo(input3);
+            Console.WriteLine(add2);
+            Console.ReadLine();
+        }
+    }
+}
