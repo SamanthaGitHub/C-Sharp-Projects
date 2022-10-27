b2ab21744d66d25addaa737a7cc13a86875aaeb9
Samantha <samanthax0326@gmail.com>
2022-10-27 16:12:07 -0700

    update

--- MainMethod/MainMethod/Multiply.cs
+++ MainMethod/MainMethod/Multiply.cs
@@ -9,26 +9,26 @@ namespace MainMethod
     class Multiply
     {
         // My First Method: multiply
-        public static int MathMethod(int input1) // *3
+        public int MathMethod(int input1) // *3
         {
             int result1 = input1 * 3;
             return result1;
         }
 
         // My Second Method: divide
-        public static decimal MathMethod(decimal input2, decimal nothing = 0) // /2
+        public int MathMethod(decimal input2, decimal nothing = 0) // /2
         {
-            decimal result2 = input2 / 2;
+            int result2 = Convert.ToInt32(input2 / 2);
             return result2;
         }
 
         // My Third Method: string conversion
-        public static string MathMethod(string input)
+        public int MathMethod(string input)
         {
             int input3 = Convert.ToInt32(input);
             int result3 = input3 + 2;
-            string result = Convert.ToString(result3);
-            return result;
+            //string result = Convert.ToString(result3);
+            return result3;
         }
     }
 }
--- MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/Program.cs
@@ -8,23 +8,21 @@ namespace MainMethod
         static void Main(string[] args)
         {
             // Multiply by 3 method
-            new Multiply(); //instatiating the class
+            Multiply multi1 = new Multiply(); //instatiating the class
             Console.WriteLine("What does 4 multiplied by 3 equal?");
-            int myNum = Multiply.MathMethod(4);
+            int myNum = multi1.MathMethod(4);
             Console.WriteLine(myNum); //prints result 12
             Console.ReadLine();
 
             // Divide by 2 method
-            new Multiply(); //instatiating the class
             Console.WriteLine("What does 7 divided by 2 equal?");
-            decimal myNum2 = Multiply.MathMethod(7, 0);
+            decimal myNum2 = multi1.MathMethod(7m, 0m);
             Console.WriteLine(myNum2); //prints result 3.5
             Console.ReadLine();
 
             // String conversion method
-            new Multiply(); //instatiating the class
             Console.WriteLine("What does 11 plus 2 equal?");
-            int myNum3 = Convert.ToInt32(Multiply.MathMethod("11")); //converts to integer
+            int myNum3 = Convert.ToInt32(multi1.MathMethod("11")); //converts to integer
             Console.WriteLine(myNum3); //prints result 13
             Console.ReadLine();
         }
