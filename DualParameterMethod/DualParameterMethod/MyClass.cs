5e049a392c936dc1373f536e446aeb31d5529a62
Samantha <samanthax0326@gmail.com>
2022-10-27 19:14:54 -0700

    DualParameter assignment

---
+++ DualParameterMethod/DualParameterMethod/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualParameterMethod
+{
+    class MyClass
+    {
+        public int MyMethod(int int1, int int2 = 1)
+        {
+            int result = (int1 + 3) * int2;
+            return result;
+        }
+    }
+}
---
+++ DualParameterMethod/DualParameterMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DualParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass Class = new MyClass();
+
+            try
+            {
+                Console.WriteLine("Please select a number:");
+                int int1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("If you would like to, enter a second number:");
+                string integer2 = Console.ReadLine();
+
+                if (integer2 != "")
+                {
+                    int opint = Convert.ToInt32(integer2);
+                    Console.WriteLine(Class.MyMethod(int1, opint));
+                }
+                else
+                {
+                    Console.WriteLine(Class.MyMethod(int1));
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid integer");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
