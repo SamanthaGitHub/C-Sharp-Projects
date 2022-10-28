874ba88b6984c19e6bc5f2538076093f378d2c32
Samantha <samanthax0326@gmail.com>
2022-10-27 19:53:10 -0700

    MethodClass assignment

---
+++ 281MethodClass/281MethodClass/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace _281MethodClass
+{
+    public class MyClass
+    {
+         public void MyMethod(int int1, int int2)
+        {
+            int1 += 5; //does math on int1
+            Console.WriteLine(int2); //prints out int2
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 281MethodClass/281MethodClass/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _281MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass Class = new MyClass(); //instatiating the class as Class
+            Class.MyMethod(3, 4); //passing in two numbers; will print 4
+            Class.MyMethod(int1: 2, int2: 5); //specify parameters by name; will print 5
+        }
+    }
+}
