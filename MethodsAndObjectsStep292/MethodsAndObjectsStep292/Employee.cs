9e8fe7df96c5960811ce476946266cb3cbd249e1
Samantha <samanthax0326@gmail.com>
2022-10-28 15:30:51 -0700

    MethodsAndObjects assignment

---
+++ MethodsAndObjectsStep292/MethodsAndObjectsStep292/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsStep292
+{
+    class Employee : Person //inheritance
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ MethodsAndObjectsStep292/MethodsAndObjectsStep292/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsStep292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee(); //instatiation
+            worker.FirstName = "Sample";
+            worker.LastName = "Student";
+            worker.SayName(); //calls inherited method
+            Console.ReadLine();
+        }
+    }
+}
