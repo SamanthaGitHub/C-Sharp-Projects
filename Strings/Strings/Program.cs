fc99414b5ea2168ef67b6666958d699b7ab04f52
Samantha <samanthax0326@gmail.com>
2022-10-27 19:19:51 -0700

    strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            // STRINGS
+            // StringBuilder
+            //StringBuilder sb = new StringBuilder("I love ", 25);
+            //sb.Append("cheese.");
+            //Console.WriteLine(sb);
+            //Console.ReadLine();
+
+            // ARRAYS
+            //int[] numArray = new int[5]; //declares an array with length of 5
+            //numArray[0] = 5;
+            //numArray[1] = 6;
+            //numArray[2] = 7;
+            //numArray[3] = 8;
+            //numArray[4] = 9;
+
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 300 };
+
+            //Console.WriteLine(numArray1[3]);
+            //Console.ReadLine();
+
+            // LISTS
+            List<int> intList = new List<int>();
+            intList.Add(4)
+
+        }
+    }
+}
