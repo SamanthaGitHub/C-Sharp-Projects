095e12b55722efc1e914848381ae4390ee40dd93
Samantha <samanthax0326@gmail.com>
2022-10-29 12:48:31 -0700

    Polymorphism assignment

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Employee : Person, IQuittable //inheritance
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName); //outputs full name
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I quit.");
+        }
+
+    }
+}
---
+++ Polymorphism/Polymorphism/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class Person //abstract class
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            return;
+        }
+    }
+}
