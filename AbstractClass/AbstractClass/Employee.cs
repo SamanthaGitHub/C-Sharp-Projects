3c2e17e6ca46c352a055ce57088e858f4d94029e
Samantha <samanthax0326@gmail.com>
2022-10-29 11:53:37 -0700

    AbstractClass assignment

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Employee : Person //inheritance
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName); //outputs full name
+        }
+        
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee(); //instatiation
+            worker.firstName = "Sample";
+            worker.lastName = "Student";
+            worker.SayName(); //calls inherited method
+            Console.ReadLine();
+        }
+    }
+}
