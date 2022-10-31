90e4f693ca253e25dba53e7db3ad5a0cc9dd84ac
Samantha <samanthax0326@gmail.com>
2022-10-31 13:47:14 -0700

    fixed

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public bool isEqual { get; set; }
+
+
+        //overoading "=="
+        public static bool operator == (Employee emp1, Employee emp2) //checks for equality
+        {
+            if (emp1.ID == emp2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee emp1, Employee emp2) //checks for inequality
+        {
+            //bool isEqual = emp1.ID != emp2.ID;
+            //return Console.WriteLine(isEqual);
+            if (emp1.ID != emp2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
