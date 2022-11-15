988ae307f9f35ab9e4861d9a85fca598e2d93204
Samantha <samanthax0326@gmail.com>
2022-11-14 16:38:32 -0800

    entity update

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                //LINQ version of above line
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups) //previously broke here for a an issue with mapping conceptually
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities())//establishes connx to db
+            {
+                var signup = db.SignUps.Find(Id); //finds the element
+                signup.Removed = DateTime.Now; //adds in the time of removal
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
